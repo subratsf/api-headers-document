87e362cf691504834b7f89b4065c88d998cf7519
Pawel Psztyc <jarrodek@gmail.com>
2018-08-27 21:09:30 -0700

    Update: Adding support for compact json-ld model

--- demo/model.js
+++ demo/model.js
@@ -1,23 +1,7 @@
-const amf = require('amf-client-js');
-const fs = require('fs');
+const generator = require('@api-components/api-model-generator');
 
-amf.plugins.document.WebApi.register();
-amf.plugins.document.Vocabularies.register();
-amf.plugins.features.AMFValidation.register();
+const files = new Map();
+files.set('demo-api/demo-api.raml', 'RAML 1.0');
 
-amf.Core.init().then(() => {
-  const ramlParser = amf.Core.parser('RAML 1.0', 'application/yaml');
-  const jsonLdParser = amf.Core.generator('AMF Graph', 'application/ld+json');
-  ramlParser.parseFileAsync('file://demo/api.raml')
-  .then((doc) => {
-    const r = amf.Core.resolver('RAML 1.0');
-    doc = r.resolve(doc, 'editing');
-    return jsonLdParser.generateString(doc);
-  })
-  .then((data) => {
-    fs.writeFileSync('demo/amf-model.json', data, 'utf8');
-  })
-  .catch((cause) => {
-    console.error(cause);
-  });
-});
+generator(files)
+.then(() => console.log('Finito'));
